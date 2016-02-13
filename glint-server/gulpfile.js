3baab0903139d57a4e8a3a42c0fc91ac31de21cd
Michael Laccetti <michael@laccetti.com>
2016-02-13 00:19:46 -0500

    i dunno

--- glint-server/gulpfile.js
+++ glint-server/gulpfile.js
@@ -1,3 +1,9 @@
+//TODO remove when harmony_rest_parameters is enabled by default
+require('harmonize')(['harmony_rest_parameters']);
+
+//TODO remove when decorators land in node
+require('babel-register');
+
 const gulp = require('gulp');
 
 // plugins
@@ -13,6 +19,16 @@ gulp.task('watch', () => {
     ext:    'js html',
     env:    {'NODE_ENV': 'development'}
   });
+});
+
+gulp.task('test', () => {
+  return gulp.src('./test/unit/**/*.es6')
+    .pipe(mocha({
+      reporter: 'spec',
+      quiet:false,
+      colors:true,
+      timeout: 10000
+    }));
 });
 
 gulp.task('default', ['watch']);
