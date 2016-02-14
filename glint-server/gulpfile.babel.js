fc3f27656a0957ae161cd9c5a51c0914c35dec7d
Michael Laccetti <michael@laccetti.com>
2016-02-13 22:41:07 -0500

    Add in the linting - see if fixing the warnings fixes the testing?

--- glint-server/gulpfile.babel.js
+++ glint-server/gulpfile.babel.js
@@ -1,28 +1,39 @@
+// require("harmonize")();
+
 // gulp
 import gulp from 'gulp';
 
 // plugins
+import eslint from 'gulp-eslint';
 import nodemon from 'gulp-nodemon';
 
 // testing
 import mocha from 'gulp-mocha';
 
-gulp.task('test', () => {
+
+gulp.task('lint', () => {
+  return gulp.src(['./src/**/*.js', './test/**/*.js', 'gulpfile.js'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+gulp.task('test', ['lint'], () => {
   return gulp.src('./test/unit/**/*.js')
     .pipe(mocha({
-      reporter:  'spec',
-      quiet:     false,
-      colors:    true,
-      timeout:   10000
+      reporter: 'html',
+      quiet: false,
+      colors: true,
+      timeout: 10000
     }));
 });
 
 gulp.task('watch', () => {
   nodemon({
     script: __dirname + '/index.js',
-    cwd:    __dirname,
-    ext:    'js html',
-    env:    {'NODE_ENV': 'development'}
+    cwd: __dirname,
+    ext: 'js html',
+    env: {'NODE_ENV': 'development'}
   });
 });
 
