6846c053800a269c67502255689bfcaf4134896f
Michael Laccetti <michael@laccetti.com>
2016-08-22 18:23:17 -0400

    Migrating the monorepo into different projects; this is glint-dashboard

--- configure.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p ~/.npm-local
-echo "prefix = ~/.npm-local" | tee ~/.npmrc
-chmod 0600 ~/.npmrc
--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-const hub = require('gulp-hub');
-
-// dashboard: 'glint-dashboard/gulpfile.js',
-const projects = {
-  library: 'glint-lib/gulpfile.js',
-  server: 'glint-server/gulpfile.js'
-};
-
-hub([projects.dashboard, projects.library, projects.server]);
